e41f2a45d26b3af6927fe81e63d49cf7b35f5160
Rikitu13 <recto_jasonjay@plpasig.edu.ph>
2025-05-23 16:21:15 +0800

    Initial commit of backend

--- train_reset.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras import backend as K
-import os
-
-K.clear_session()
-
-if os.path.exists("resnet50_authenticity.h5"):
-    os.remove("resnet50_authenticity.h5")
-if os.path.exists("label_encoder.pkl"):
-    os.remove("label_encoder.pkl")

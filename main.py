1c23010145e599fefede241c5757ea16647af62a
Rikitu13 <recto_jasonjay@plpasig.edu.ph>
2025-05-25 18:58:29 +0800

    updated to accept https

--- main.py
+++ main.py
@@ -214,6 +214,6 @@ def verify():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
 else:
     application = app
